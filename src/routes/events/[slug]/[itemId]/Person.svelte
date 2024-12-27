144ecd1d50dd08ffd3fbe6e80decf91f87cdf333
alonot <priyanshup27092004@gmail.com>
2024-12-28 00:27:03 +0530

    image fix

--- src/routes/events/[slug]/[itemId]/Person.svelte
+++ src/routes/events/[slug]/[itemId]/Person.svelte
@@ -1,56 +1,64 @@
 <script lang="ts">
-    export let name = 'John Doe'
-    export let phone = '123-456-7890'
+    export let name = "John Doe";
+    export let phone = "123-456-7890";
     import { onMount } from "svelte";
 
-    let url = 'https://picsum.photos/200/300'
-    let origin = "https://finance-petrichor.vercel.app"
+    let url = "https://www.svgrepo.com/download/454078/account.svg";
+    let origin = "https://finance-petrichor.vercel.app";
 
-     onMount(() => {
-        if (window.top.location.origin)
-            origin = window.top.location.origin
-    
+    onMount(async () => {
+        if (window.top.location.origin) origin = window.top.location.origin;
+
         // img_div = document.getElementById("back_bg") as HTMLDivElement;
-        if (phone != '123-456-7890') {
-            url = `${origin}/uploads/${name.toLowerCase()}.png`
-            console.log("Origin:", origin)
-
-            if (origin ==  "https://finance-petrichor.vercel.app" || origin == "http://localhost:5173"){
-            console.log("Fetching image")
-            fetch('http://localhost:8000/internal/image/', {
-                    method: 'POST',
+        if (phone != "123-456-7890") {
+            console.log("Origin:", origin);
+            
+            if (
+                origin == "https://finance-petrichor.vercel.app" ||
+                origin == "http://localhost:5173"
+            ) {
+                console.log("Fetching image", origin);
+                await fetch("http://localhost:8000/internal/image/", {
+                    method: "POST",
                     headers: {
-                        'Content-type': 'application/json',
+                        "Content-type": "application/json",
                     },
-                    credentials: 'include',
-                    mode: 'cors',
+                    credentials: "include",
+                    mode: "cors",
                     body: JSON.stringify({
-                        "name":name,
-                        "password": process.env.pass
-                    })
-                }).then(res => res.json())
-                .then(res => {
-                console.log("Got Image")
+                        name: name
+                    }),
+                })
+                .then((res) => res.json())
+                .then((res) => {
+                    console.log("Got Image");
                     const imageURL = `data:image/png;base64,${res.image}`;
                     url = `${imageURL}`;
-                }).catch(err => {
-                    console.log("image fetch error: ",err.toString())
                 })
+                .catch((err) => {
+                    console.log("image fetch error: ", err.toString());
+                });
+            } else {
+                url = `${origin}/uploads/${name.toLowerCase()}.png`;
             }
+        } else {
+            url = "https://picsum.photos/200/300"
         }
-    })
+    });
 </script>
 
 <div class="main">
-    <div class="backpic" id="back_bg" style="background-image: url('{url}');">
-    </div>
+    <div
+        class="backpic"
+        id="back_bg"
+        style="background-image: url('{url}');"
+    ></div>
     <h2>{name}</h2>
     <p>{phone}</p>
 </div>
 
-
 <style>
-    .main{
+    .main {
         background-color: rgb(27, 27, 27, 0.5);
         backdrop-filter: blur(12px);
         font-family: var(--pfont);
@@ -60,7 +68,7 @@
         overflow: hidden;
         width: 14em;
     }
-    .backpic{
+    .backpic {
         background-size: cover;
         background-position: center;
         position: relative;
@@ -70,14 +78,14 @@
         margin: 1rem;
         background-repeat: no-repeat;
     }
-    h2{
+    h2 {
         font-family: var(--pfont);
         margin-left: 1rem;
         width: max-content;
         /* color: black; */
-    } 
-    p{
-        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+    p {
+        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
         letter-spacing: 0.05em;
         margin: 1rem;
         margin-top: -0.5rem;
