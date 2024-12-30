c5b2310d4d87adda1a5aef07509469eb6455d27f
alonot <priyanshup27092004@gmail.com>
2024-12-31 02:39:58 +0530

    info not visible fix

--- src/routes/events/[slug]/[itemId]/+page.svelte
+++ src/routes/events/[slug]/[itemId]/+page.svelte
@@ -278,8 +278,8 @@
             </span>
             <input hidden name="type" value={data.type} />
             <span class="isTeam">
-                <p>isTeam: <br/><li>if set to <strong>false</strong> then above fee is taken as <strong>per participants fee</strong> i.e. amount taken by user = event.fee * no_of_participants. </li>
-                    <br/> <li>if set to <strong>true</strong> then above fee is taken as <strong>'per team fee'</strong> i.e. amount taken by user = event.fee(the number of participants is not taken into fee calculations) </li></p>
+                <p>isTeam: <br/>if set to <strong>false</strong> then above fee is taken as <strong>per participants fee</strong> i.e. amount taken by user = event.fee * no_of_participants.
+                    <br/> if set to <strong>true</strong> then above fee is taken as <strong>'per team fee'</strong> i.e. amount taken by user = event.fee(the number of participants is not taken into fee calculations)</p>
                 <div style="display: flex;flex-direction:column">
                     <label>
                         <input
