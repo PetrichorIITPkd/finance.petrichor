69216ff56261904ed42ebb45f8c85fa6f9071e50
Simeonk1307 <112301031@smail.iitpkd.ac.in>
2026-01-03 23:56:36 +0530

    necessary changes for year to be included in eventId generation.

--- src/routes/events/[slug]/[itemId]/+page.server.ts
+++ src/routes/events/[slug]/[itemId]/+page.server.ts
@@ -9,6 +9,9 @@ import { build } from 'esbuild';
 import type { PageServerLoad } from './$types';
 import { fail, error } from '@sveltejs/kit';
 
+// TODO Update year as needed
+const YEAR = 2026;
+
 export const load: PageServerLoad = async ({ params, url }) => {
     if (params.slug != process.env.pass) {
         throw error(404, { message: "Wrong Password" })
@@ -27,7 +30,8 @@ export const load: PageServerLoad = async ({ params, url }) => {
         if (result.status != 200) {
             throw error(404, { message: 'Unable to resolve the response: ' + result.message })
         }
-        default_event.eventId = `${event_type?.at(0)}F${result.data.toString().padStart(2, '0')}`
+        // # TODO Update year as needed
+        default_event.eventId = `${event_type?.at(0)}F${result.data.toString().padStart(2, '0')}_${YEAR}`
         // default_event.markdown = fs.readFileSync(path.resolve('./src/routes/events/[slug]/[itemId]/_page.svx')).toString()
         return {
             "event": default_event,
